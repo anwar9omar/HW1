c919a818c96937d3359532a5d325e8171eeabd46
ANWAR OMAR <116513180+anwar9omar@users.noreply.github.com>
2022-10-24 19:51:40 +0300

    Add files via upload

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+   public String title;
+   public String author;
+
+
+   public void printTitle(){
+       System.out.println(title);
+   }
+    public void printAuthor(){
+        System.out.println(author);
+    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        Book bo=new Book();
+        bo.printTitle();
+        bo.printAuthor();
+
+
+
+
+
+    }
+}
